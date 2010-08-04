57ee1ee73d89d4f8ad135344fda4895b35987de1
Vlasta Neubauer <@paranoiq>
2010-08-04 21:57:35 +0200

    load local CSV files

--- mantra/app/presenters/ImportPresenter.php
+++ mantra/app/presenters/ImportPresenter.php
@@ -6,22 +6,35 @@ use Nette\Forms\ISubmitterControl;
 
 class ImportPresenter extends BasePresenter {
     
-    public function createComponentForm() {
-        $form = FormFactory::create($this, 'form');
+    public function createComponentUploadForm() {
+        $form = FormFactory::create($this, 'uploadForm');
         
-        $form->addGroup();
+        $maxSize = ((int) ini_get('upload_max_filesize') - 1) << 20;
+        
+        $form->addGroup('Upload file');
         
         $form->addFile('file', 'File')
             ->addRule(Form::FILLED, 'Select the file to import')
-            ->addRule(Form::MIME_TYPE, 'File must be either JSON or CSV.', 'application/json,text/csv,application/octet-stream');
+            ->addRule(Form::MAX_FILE_SIZE, "File cannot be larger than $maxSize", $maxSize)
+            ->addRule(Form::MIME_TYPE, 'File must be either JSON or  CSV.', 'application/json,text/csv,application/octet-stream');
         $form->addSelect('type', 'Type', array('csv,' => 'CSV ,', 'csv;' => 'CSV ;'/*, 'json' => 'JSON'*/));
         
-        $form->addSubmit('import', 'Import')->onClick[] = array($this, 'importFile');
-        
-        
+        $form->addSubmit('upload', 'Import')->onClick[] = array($this, 'uploadFile');
     }
     
-    public function importFile(ISubmitterControl $button) {
+    public function createComponentLocalForm() {
+        $form = FormFactory::create($this, 'localForm');
+        
+        $form->addGroup('Load local file');
+        
+        $form->addText('file', 'File', 60)
+            ->addRule(Form::FILLED, 'Pleas fill in the path to file.');
+        $form->addSelect('type', 'Type', array('csv,' => 'CSV ,', 'csv;' => 'CSV ;'/*, 'json' => 'JSON'*/));
+        
+        $form->addSubmit('local', 'Import')->onClick[] = array($this, 'localFile');
+    }
+    
+    public function uploadFile(ISubmitterControl $button) {
         $values = $button->parent->getValues();
         
         if ($values['file']->getError() != 0) {
@@ -33,6 +46,30 @@ class ImportPresenter extends BasePresenter {
             $count = $this->importCsv($values['file']->getTemporaryFile(), substr($values['type'], 3, 1));
         } else {
             $count = $this->importJson($values['file']->getTemporaryFile());
+        }
+        
+        $this->flashMessage("File was succesfully loaded ($count items).");
+        
+        $this->redirect('Select:default');
+    }
+    
+    public function localFile(ISubmitterControl $button) {
+        $values = $button->parent->getValues();
+        
+        if (!file_exists($values['file'])) {
+            $button->parent->addError('File not found.');
+            return;
+        }
+        
+        if (!is_readable($values['file'])) {
+            $button->parent->addError('File cannot be read from.');
+            return;
+        }
+        
+        if (substr($values['type'], 0, 3) == 'csv') {
+            $count = $this->importCsv($values['file'], substr($values['type'], 3, 1));
+        } else {
+            $count = $this->importJson($values['file']);
         }
         
         $this->flashMessage("File was succesfully loaded ($count items).");
@@ -57,7 +94,7 @@ class ImportPresenter extends BasePresenter {
             }
             $batch[] = $obj;
             
-            if ($n > 1000) {
+            if ($b > 1000) {
                 $this->db->batchInsert($batch, $this->collection, $this->database);
                 $b = 0;
                 $batch = array();
