e2127c4173c0acde4a85de4c9e0ec8294b255747
Vlasta Neubauer <@paranoiq>
2010-08-04 13:11:45 +0200

    show and kill process

--- mantra/app/presenters/ProcessPresenter.php
+++ mantra/app/presenters/ProcessPresenter.php
@@ -1,15 +1,53 @@
 <?php
 
 use Mantra\FormFactory;
-use Phongo\Tools;
+use Mantra\Formater;
 use Nette\Forms\ISubmitterControl;
 
 class ProcessPresenter extends BasePresenter {
     
     public function actionDefault() {
+        $formater = new Formater;
+        $formater->html = TRUE;
         
-        $this->db->getProcessList();
+        $processList = $this->db->getProcessList();
+        foreach ($processList as $key => $process) {
+            $processList[$key]['query'] = isset($process['query']) ? $formater->formatJson($process['query'] ?: new StdClass) : '';
+            $processList[$key]['secs_running'] = isset($process['secs_running']) ?: 0;
+        }
         
+        $this->template->processList = $processList;
+        
+        $this->template->form = $this->getComponent('form');
+    }
+    
+    public function createComponentForm() {
+        $form = FormFactory::create($this, 'form');
+        
+        $container = $form->addContainer('process');
+        foreach ($this->template->processList as $process) {
+            $container->addCheckbox($process['opid']);
+        }
+        
+        $form->addSubmit('kill', "Kill process")->onClick[] = array($this, 'killProcess');
+        
+        $form->addProtection('Protection timeout expired. Pleas, try again.');
+        
+        return $form;
+    }
+    
+    public function killProcess(ISubmitterControl $button) {
+        $values = $button->parent->getValues();
+        
+        foreach ($values['process'] as $processId => $checked) {
+            if (!$checked) continue;
+            
+            $this->db->terminateProcess($processId);
+            
+            $this->flashMessage("Process '$processId' was killed.");
+        }
+        
+        $this->redirect('Process:default');
     }
     
 }
