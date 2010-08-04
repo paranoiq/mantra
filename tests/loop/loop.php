58ae5b5eeebc12a1ab4f90a060c3d9219b057e19
Vlasta Neubauer <@paranoiq>
2010-08-04 10:59:34 +0200

    testing loop

---
+++ tests/loop/loop.php
@@ -0,0 +1,21 @@
+<?php
+
+use Nette\Environment;
+
+require_once '../bootstrap.php';
+
+$db = Environment::getService('Phongo\IConnection');
+$db->connect();
+$db->setSafeMode();
+
+set_time_limit(0);
+
+$db->selectCollection('abcd', 'test');
+
+$n = 100000;
+while ($n > 0) {
+    $result = $db->find(array());
+    $result->fetchAll();
+    $n--;
+    echo $n . "<br>";
+}
